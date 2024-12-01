dbc9281d19be1ef99be9e7ca1937140092159420
umut <diler.2029@gmail.com>
2024-12-02 02:06:58 +0300

    search Screen ui created

--- app/src/main/java/com/example/imdbapp/ui/screens/search/SearchFragment.kt
+++ app/src/main/java/com/example/imdbapp/ui/screens/search/SearchFragment.kt
@@ -21,5 +21,9 @@ class SearchFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
 
 }
